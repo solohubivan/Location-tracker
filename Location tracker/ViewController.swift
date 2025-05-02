94a0da6df5447656a268b5cc4728fcefee596588
solohubivan <solohubivan@gmail.com>
2025-05-02 21:50:54 +0300

    Initial Commit

---
+++ Location tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location tracker
+//
+//  Created by Ivan Solohub on 02.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
